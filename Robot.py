40f00b2dcd10597957775a591ff797c207db947f
jonnaknibestol <jonna.knibestol@gmail.com>
2016-09-07 14:02:01 +0200

    Skrev om programmet med klass med konstruktor

--- Robot.py
+++ Robot.py
@@ -3,21 +3,22 @@ class Product:
 	count = 0
 	tax = 0
 
-class Product: 
-		def price_with_tax(self):
-				return self.price * self.count * self.tax
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
 
-robot = Product()
-robot.price = 900
-robot.count = 2
-robot.tax = 1.25
+	def __init__(self, price, count, tax):
+		self.price = price 
+		self.count = count
+		self.tax = tax 
 
-book = Product()
-book.price = 100
-book.count = 1 
-book.tax = 1.06
+robot = Product(price=900, count=2, tax=1.25)
+
+book = Product(price=100, count=1, tax=1.06)
 
 print (robot.price_with_tax() + book.price_with_tax())
+
+
+
 
 
 
