7e1c04c7d670160e2298cd7e773af4d0a535376c
jonnaknibestol <jonna.knibestol@gmail.com>
2016-09-07 11:44:34 +0200

    Skrev om programmet med dictionaries

--- Robot.py
+++ Robot.py
@@ -9,17 +9,17 @@ print (customers_by_name["bob"])
 print (customers_by_name["anne"])
 
 
-
-
-
-
-
 robot_price = 900
 robot_count = 2
 robot_tax = 1.25
 book_price = 100
 book_tax = 1.06
 print(robot_price * robot_count * robot_tax + book_price * book_tax)
+
+robot = {"price":900, "count":2, "tax":1.25}
+book = {"price":100, "tax":1.06}
+print(robot["price"] * robot["count"] * robot["tax"] + book["price"] * book["tax"])
+
 
 
 
