ded7a07ed132c74477df3846324659ecb81e4e8d
jonnaknibestol <jonna.knibestol@gmail.com>
2016-09-07 11:52:23 +0200

    Skrev om problemet med en klass

--- Robot.py
+++ Robot.py
@@ -8,17 +8,22 @@ customers_by_name = { "bob": "45", "anne": "25"}
 print (customers_by_name["bob"])
 print (customers_by_name["anne"])
 
+class Product: 
+	price = 0
+	count = 0
+	tax = 1
 
-robot_price = 900
-robot_count = 2
-robot_tax = 1.25
-book_price = 100
-book_tax = 1.06
-print(robot_price * robot_count * robot_tax + book_price * book_tax)
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
 
-robot = {"price":900, "count":2, "tax":1.25}
-book = {"price":100, "tax":1.06}
-print(robot["price"] * robot["count"] * robot["tax"] + book["price"] * book["tax"])
+book = Product()
+book.price = 100 
+book.tax = 1.06
+
+print(robot.price * robot.count * robot.tax + book.price * book.tax)
+
 
 
 
